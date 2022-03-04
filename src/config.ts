a3ccf5af3c0c5f596f76b276151b3efb9fa20161
Febzey <braydent1235@gmail.com>
2022-03-03 21:42:17 -0600

    Added current TPs

--- src/config.ts
+++ src/config.ts
@@ -6,31 +6,6 @@ import { readFile } from 'fs/promises';
 const configFile: any = await readFile('./config.json');
 export const bot_config = JSON.parse(configFile);
 
-// export const bot_config = {
-//     mainChannelId:      configfile.mainChannelId,
-//     whitelist:          configfile.whitelist,
-//     advertiseTime:      configfile.advertiseTime,
-//     advertise:          configfile.advertise,
-//     visualRange:        configfile.visualRange,
-//     secretChannelId:    configfile.secretChannelId,
-//     liveViewer:         configfile.liveViewer,
-//     liveViewerPort:     configfile.liveViewerPort,
-//     antiAfk:            configfile.antiAfk,
-//     websocket:          configfile.websocket,
-//     logTps:             configfile.logTps,
-//     logPlayerCount:     configfile.logPlayerCount,
-//     infoJoinMessages:   configfile.infoJoinMessages,
-//     welcomeMessages:    configfile.welcomeMessages,
-//     loadCommands:       configfile.loadCommands,
-//     Discord_whitelist:  configfile.Discord_whitelist,
-//     saveTablist:        configfile.saveTablist,
-//     useSecretChannel:   configfile.useSecretChannel,
-//     server:             configfile.server,
-//     websocket_port:     configfile.websocket_port,
-//     websocket_livechat: configfile.websocket_livechat,
-//     websocket_unqiueplayers: confi
-// };
-
 export const apiUrl: string = bot_config.apiUrl;
 export const serverString: string = bot_config.server
 
--- src/websocket/websocket.ts
+++ src/websocket/websocket.ts
@@ -4,6 +4,7 @@ import { bot_config } from '../config.js';
 import { BotEvents } from 'mineflayer';
 import { promisify } from 'util';
 import database from '../database/createPool.js';
+import { __Tps } from "../tps/gettps.js";
 
 const promisedQuery = promisify(database.query).bind(database);
 
@@ -15,14 +16,27 @@ const webSocket = async () => {
 
     const uniquePlayers = await promisedQuery(`SELECT COUNT(*) as cnt FROM users`)
 
-    wss.on("connection", client => {
-        client.send(JSON.stringify({playerlist: Object.keys(bot.players), uniquePlayers: uniquePlayers[0].cnt ? uniquePlayers[0].cnt : null}));
-        client.on("message", (content: any) => {
+    wss.on("connection", (client, request) => {
+        if (!bot_config.allowedOrigins.includes(request.headers.origin)) return client.close();
+      
+        let tps: number;
 
+        if (process.uptime() / 60 > 2) {
+            const min = () => __Tps.reduce((m, c) => c[0] < m[0] ? c : m, __Tps[0]);
+            const TPS = min().toString().split(",");
+            tps = parseInt(TPS[0]);
+        }
+
+        client.send(JSON.stringify({playerlist: Object.keys(bot.players), uniquePlayers: uniquePlayers[0].cnt ? uniquePlayers[0].cnt : null, tps: tps ? tps : 20}));
+
+        client.on("error", console.log);
+
+        if (!bot_config.websocket_livechat) return;
+        client.on("message", (content: any) => {
             if (!content.toString()) return;
             bot.chat(`[W]${content.toString()}`)
         })
-
+    
     });
 
     if (!bot_config.websocket_livechat) return;
@@ -30,9 +44,7 @@ const webSocket = async () => {
         if (!wss.clients) return;
         const username: string = content[0][0],
               message: string = content[0][1];
-        wss.clients.forEach(set => {
-            return set.send(JSON.stringify({user: username, msg: message}));
-        })
+        wss.clients.forEach(set => set.send(JSON.stringify({user: username, msg: message})));
     })
 
 
